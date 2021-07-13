4f2313ac9d6bffbf9b3fd9d23ea042d98ad900f1
christopherhero <christopherheroo@gmail.com>
2021-07-13 15:29:26 +0200

    [fix github actions]

--- tests/Application/config/bundles.php
+++ tests/Application/config/bundles.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
@@ -54,6 +56,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Sylius\Bundle\ApiBundle\SyliusApiBundle::class => ['all' => true],
     SyliusLabs\DoctrineMigrationsExtraBundle\SyliusLabsDoctrineMigrationsExtraBundle::class => ['all' => true],
-    Symplify\ConsoleColorDiff\ConsoleColorDiffBundle::class => ['dev' => true, 'test' => true],
     FOS\ElasticaBundle\FOSElasticaBundle::class => ['all' => true],
 ];
