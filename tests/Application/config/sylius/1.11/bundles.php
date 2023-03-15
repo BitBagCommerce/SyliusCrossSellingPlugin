5df781ded9e43bdc1b369dc6312c37b559d1e0dd
Rafal Malenta <rafal.malenta.extern@foodspring.com>
2023-03-15 13:00:49 +0100

    Added dot

--- tests/Application/config/sylius/1.11/bundles.php
+++ tests/Application/config/sylius/1.11/bundles.php
@@ -6,4 +6,5 @@ return [
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
     Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true]
 ];
