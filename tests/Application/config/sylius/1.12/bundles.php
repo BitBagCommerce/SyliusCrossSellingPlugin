0a8cdf4b558d18cba4acc1c44b0de0c576c44c7d
Rafal Malenta <rafal.malenta.extern@foodspring.com>
2023-03-15 12:50:32 +0100

    Added dot

--- tests/Application/config/sylius/1.12/bundles.php
+++ tests/Application/config/sylius/1.12/bundles.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 return [
     League\FlysystemBundle\FlysystemBundle::class =>['all' => true],
     Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
+//    Sylius\Bundle\ShopBundle\SyliusShopBundle::class =>['all' => true]
 //    Sylius\Bundle\ApiBundle\SyliusApiBundle::class => ['all' => true]
 //    Sylius\Bundle\MailerBundle\SyliusMailerBundle::class => ['all' => true]
 ];
