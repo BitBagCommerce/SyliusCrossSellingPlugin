aea3499824a2e64e15e93091fa5108232fa33f36
Rafal Malenta <rafal.malenta.extern@foodspring.com>
2023-03-16 09:01:15 +0100

    Added support for symfony 6

--- tests/Application/config/sylius/1.12/bundles.php
+++ tests/Application/config/sylius/1.12/bundles.php
@@ -4,5 +4,7 @@ declare(strict_types=1);
 
 return [
     League\FlysystemBundle\FlysystemBundle::class =>['all' => true],
-    Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true]
+    Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
+    SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
+    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
 ];
